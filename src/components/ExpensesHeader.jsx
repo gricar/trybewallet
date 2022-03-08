8124231b753176baeb7b78a2079ec9846e4c0875
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-03-08 11:41:48 -0300

    incluido thead na tabela, conforme warning do DOM

--- src/components/ExpensesHeader.jsx
+++ src/components/ExpensesHeader.jsx
@@ -5,35 +5,37 @@ class ExpensesHeader extends React.Component {
     return (
       <table>
         <caption>Expenses Table</caption>
-        <tr>
-          <th>
-            Descrição
-          </th>
-          <th>
-            Tag
-          </th>
-          <th>
-            Método de pagamento
-          </th>
-          <th>
-            Valor
-          </th>
-          <th>
-            Moeda
-          </th>
-          <th>
-            Câmbio utilizado
-          </th>
-          <th>
-            Valor convertido
-          </th>
-          <th>
-            Moeda de conversão
-          </th>
-          <th>
-            Editar/Excluir
-          </th>
-        </tr>
+        <thead>
+          <tr>
+            <th>
+              Descrição
+            </th>
+            <th>
+              Tag
+            </th>
+            <th>
+              Método de pagamento
+            </th>
+            <th>
+              Valor
+            </th>
+            <th>
+              Moeda
+            </th>
+            <th>
+              Câmbio utilizado
+            </th>
+            <th>
+              Valor convertido
+            </th>
+            <th>
+              Moeda de conversão
+            </th>
+            <th>
+              Editar/Excluir
+            </th>
+          </tr>
+        </thead>
       </table>
     );
   }
