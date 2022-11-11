da6605d575f0cef87433f11efd58b178bece2a11
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-11-11 15:41:45 -0300

    Updates

---
+++ precache-manifest.32367801f2a268b6875fd31e8fc01e38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36851b7d521e9e76e6e0a82a2f38c8f2",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "a15b069a44ba5eb4cf20",
+    "url": "/trybewallet/static/css/main.69a32c83.chunk.css"
+  },
+  {
+    "revision": "6e55313b783e3f944572",
+    "url": "/trybewallet/static/js/2.6faf7a0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.6faf7a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15b069a44ba5eb4cf20",
+    "url": "/trybewallet/static/js/main.28d658b3.chunk.js"
+  },
+  {
+    "revision": "efdc6867495475ae3922",
+    "url": "/trybewallet/static/js/runtime-main.daf72ee0.js"
+  }
+]);
